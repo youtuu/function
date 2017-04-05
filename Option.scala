99806aab24401a1e3fe39334fb0c7e03999bd0a8
youtuu <acjtmtttpj22@gmail.com>
2017-04-05 10:06:38 +0900

    first

---
+++ Option.scala
@@ -0,0 +1,29 @@
+trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(x) => x
+    case None => default
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(x) if f(x) =>  this
+    case _ => None
+  }
+
+}
+
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+
